8bdcae4bab65596740eda4727889c33c00ba2352
Al Mamun Khan <almamunkhan0101@gmail.com>
2025-08-11 20:12:46 +0600

    topbar done

---
+++ src/component/Navbar.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+        <div className="container">
+            <h2 className='font-extrabold text-xl '>
+                Exclusive
+            </h2>
+            <ul>
+    <li><a href="">Home</a></li>
+    <li><a href="">Contact</a></li>
+    <li><a href="">About</a></li>
+    <li><a href="">Sign Up</a></li>
+            </ul>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
--- src/component/Topbar.jsx
+++ src/component/Topbar.jsx
@@ -6,16 +6,16 @@ const Topbar = () => {
     <div>
       {" "}
       {/* top bar */}
-      <div className="bg-black relative  w-full mx-auto py-3 text-white justify-between flex px-10">
-        <div className="absolute left-1/2 -translate-x-1/2 flex items-center gap-2 text-sm text-center">
+      <div className="bg-black relative  w-full mx-auto py-4 text-white justify-between flex px-5 lg:px-10">
+        <div className="lg:absolute lg:left-1/2 lg:-translate-x-1/2 flex items-center gap-5 text-xs text-center">
           <h2>
             Summer SoleFor all Seim Suit and Free Express Deliver - 50%o OFF{" "}
           </h2>
-          <a href="#" className="underline text-base">
+          <a href="#" className="underline lg:text-base font-bold">
             Shop Now
           </a>
         </div>
-        <div className="ml-auto flex items-center gap-2  ">
+        <div className="hidden ml-auto sm:flex items-center gap-1 text-xs  ">
           <button>English</button>
           <IoIosArrowDown />
         </div>
