e7b33a8f8f0d6c8c49949c7868a9d14ee4fc0c3b
Al Mamun Khan <almamunkhan0101@gmail.com>
2025-08-12 18:29:10 +0600

    banner done

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -1,5 +1,8 @@
 import Link from "next/link";
 import React from "react";
+import { FaArrowRightLong } from "react-icons/fa6";
+import { GoDotFill } from "react-icons/go";
+
 
 const Header = () => {
   return (
@@ -19,9 +22,32 @@ const Header = () => {
               <Link href="/about">Health & Beauty</Link>
             </div>
           </div>
-          <div className="p-10">
-            <div className="bg-black w-full p-100"></div>
-            
+          <div className="p-5 w-full">
+            <div className="bg-black flex justify-between relative">
+              <div className="text-white p-10 pt-20">
+                <div className="flex items-center pb-5 gap-5 ">
+                  <img src="applelogo.png" alt="" />
+                  <h2 >Iphone 14 Seriess</h2>
+                </div>
+                <h1 className="w-50 text-4xl font-bold pb-10">
+                  Up to 10% of voucher
+                </h1>
+                <div className="flex items-center gap-2">
+                  <a href="" className="underline text-lg p-1">
+                    Show Now
+                  </a>
+                  <FaArrowRightLong />
+                </div>
+                <div className="absolute flex justify-center bottom-5 left-1/2 -translate-x-1/2 ">
+                  <GoDotFill className="text-gray-400 hover:ring hover:ring-white rounded-full" />
+                  <GoDotFill className="text-gray-400 hover:ring hover:ring-white rounded-full" />
+                  <GoDotFill className="text-red-400 hover:ring hover:ring-white rounded-full" />
+                  <GoDotFill className="text-gray-400 hover:ring hover:ring-white rounded-full" />
+                  <GoDotFill className="text-gray-400 hover:ring hover:ring-white rounded-full" />
+                </div>
+              </div>
+              <img src="banner.png" alt="" className=""/>
+            </div>
           </div>
         </div>
       </div>
