eec0f3874b35d2159a468d62bf515866c7c46a05
Al Mamun Khan <almamunkhan0101@gmail.com>
2025-08-18 04:08:56 +0600

    footer text done

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,13 @@
 import Best from '@/component/Best'
 import Categories from '@/component/Categories'
 import Explore from '@/component/Explore'
+import Footer from '@/component/Footer'
 import Header from '@/component/Header'
 import Navbar from '@/component/Navbar'
 import New from '@/component/New'
 import Today from '@/component/Today'
 import Topbar from '@/component/Topbar'
+
 import React from 'react'
 
 const page = () => {
@@ -19,6 +21,7 @@ const page = () => {
       <Best/>
       <Explore/>
       <New/>
+      <Footer/>
     </div>
   )
 }
--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -4,49 +4,54 @@ const Footer = () => {
   return (
     <div>
       <div className="bg-black text-white ">
-        <div className="container flex items-center justify-between">
+        <div className="container flex  justify-between py-20 px-10">
           
           {/* left side bottom */}
-          <div className="flex flex-col">
+          <div className="flex flex-col gap-2">
             <h2 className="text-2xl font-bold">Exclusive</h2>
-            <h2 className="text-xl">Subscribe</h2>
-            <h2 className="text-xl">Get 10% off your first order</h2>
+            <h2 className="">Subscribe</h2>
+            <h2 className="">Get 10% off your first order</h2>
             <div className="">
               <input type="text" placeholder="Enter your email " />
             </div>
           </div>
 
           {/* left side bottom */}
-          <div className="flex flex-col">
+          <div className="flex flex-col gap-2">
             <h2 className="text-2xl font-bold">Support</h2>
-            <h2 className="text-xl">exclusive@gmail.com111 Bijoy sarani, Dhaka,  DH 1515, Bangladesh.</h2>
-            <h2 className="text-xl">exclusive@gmail.com</h2>
-            <h2 className="text-xl">+88015-88888-9999</h2>
+            <h2 className="">  Bijoy sarani, Dhaka,  DH 1515, Bangladesh.</h2>
+            <h2 className="">exclusive@gmail.com</h2>
+            <h2 className="">+88015-88888-9999</h2>
           </div>
 
           {/* left side bottom */}
-          <div className="flex flex-col">
+          <div className="flex flex-col gap-2">
             <h2 className="text-2xl font-bold">Account</h2>
-            <h2 className="text-xl">My Account</h2>
-            <h2 className="text-xl">Login / Register</h2>
-            <h2 className="text-xl">Cart</h2>
-            <h2 className="text-xl">Wishlistart</h2>
-            <h2 className="text-xl">Shop</h2>
+            <h2 className="">My Account</h2>
+            <h2 className="">Login / Register</h2>
+            <h2 className="">Cart</h2>
+            <h2 className="">Wishlistart</h2>
+            <h2 className="">Shop</h2>
           </div>
           {/* left side bottom */}
-          <div className="flex flex-col">
+          <div className="flex flex-col gap-2">
             <h2 className="text-2xl font-bold">Quick Link</h2>
-            <h2 className="text-xl"> Privacy Policy</h2>
-            <h2 className="text-xl">Terms Of Use</h2>
-            <h2 className="text-xl">FAQ</h2>
-            <h2 className="text-xl">Contact</h2>
+            <h2 className=""> Privacy Policy</h2>
+            <h2 className="">Terms Of Use</h2>
+            <h2 className="">FAQ</h2>
+            <h2 className="">Contact</h2>
           </div>
           {/* left side bottom */}
-          <div className="flex flex-col">
+          <div className="flex flex-col gap-2">
             <h2 className="text-2xl font-bold">Download App</h2>
-            <h2 className="text-xl"> Save $3 with App New User Only</h2>
-          <div className="flex">
-            
+            <h2 className=""> Save $3 with App New User Only</h2>
+          <div className="flex items-center">
+
+            <img src="./Qrcode.png" alt="Qrcode" />
+            <div className="">
+              <img src="./playstore.png" alt="playstore" />
+              <img src="./Appstore.png" alt="playstore" />
+            </div>
           </div>
           </div>
 
