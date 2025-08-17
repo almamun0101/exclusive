11746230ac9527dc6dcf47684ec795ebe9309249
Al Mamun Khan <almamunkhan0101@gmail.com>
2025-08-18 01:59:23 +0600

    servie done

--- src/component/New.jsx
+++ src/component/New.jsx
@@ -5,7 +5,7 @@ const New = () => {
   return (
     <div className="container p-5 ">
       <Heading title={"Featured"} heading={"New Arrival"} />
-
+      {/* new Arraival */}
       <div className="flex gap-5 h-full flex-col lg:flex-row justify-between text-white">
         {/* left side */}
         <div className="bg-black relative w-full h-full px-10 pt-20">
@@ -33,7 +33,9 @@ const New = () => {
               className="w-full  h-full object-contain"
             />
             <div className="absolute bottom-5 left-5">
-              <h2 className=" text-lg lg:text-2xl font-bold">Women’s Collections</h2>
+              <h2 className=" text-lg lg:text-2xl font-bold">
+                Women’s Collections
+              </h2>
               <p className=" text-sm lg:text-base w-70 text-white/80 py-2">
                 Featured woman collections that give you another vibe.
               </p>
@@ -70,7 +72,7 @@ const New = () => {
               <div className="absolute bottom-5 left-5">
                 <h2 className="text-2xl font-bold">Perfume</h2>
                 <p className="text-sm  text-white/80 py-2">
-                 GUCCI INTENSE OUD EDP
+                  GUCCI INTENSE OUD EDP
                 </p>
                 <a href="" className="underline text-lg font-bold">
                   Shop Now
@@ -78,6 +80,37 @@ const New = () => {
               </div>
             </div>
           </div>
+        </div>
+      </div>
+
+      {/* service */}
+      <div className="flex gap-10 justify-between items-center py-40 px-20 ">
+        <div className="flex flex-col items-center gap-1">
+          <div className="bg-black rounded-full p-3 ring-gray-300 ring-10">
+            <img src="./delivery.png" alt="delivery" className="" />
+          </div>
+          <h2 className="text-lg font-bold pt-4">FREE AND FAST DELIVERY</h2>
+          <p className="font-extralight">
+            Free delivery for all orders over $140
+          </p>
+        </div>
+        <div className="flex flex-col items-center gap-1">
+          <div className="bg-black rounded-full p-3 ring-gray-300 ring-10">
+            <img src="./customerservice.png" alt="delivery" className="" />
+          </div>
+          <h2 className="text-lg font-bold pt-4">24/7 CUSTOMER SERVICE</h2>
+          <p className="font-extralight">
+           Friendly 24/7 customer support
+          </p>
+        </div>
+        <div className="flex flex-col items-center gap-1">
+          <div className="bg-black rounded-full p-3 ring-gray-300 ring-10">
+            <img src="./shield-tick.png" alt="delivery" className="" />
+          </div>
+          <h2 className="text-lg font-bold pt-4">MONEY BACK GUARANTEE</h2>
+          <p className="font-extralight">
+            We reurn money within 30 days
+          </p>
         </div>
       </div>
     </div>
