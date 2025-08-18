0bc34364e4a0ad4ca6973dd13837453817f8f2bd
Al Mamun Khan <almamunkhan0101@gmail.com>
2025-08-19 02:34:31 +0600

    sign in done

--- src/app/signin/page.jsx
+++ src/app/signin/page.jsx
@@ -4,9 +4,14 @@ import Navbar from "@/component/Navbar";
 import Topbar from "@/component/Topbar";
 import React, { useState } from "react";
 import firebaseConfig from "@/firebase.config";
-import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import { getAuth, signInWithEmailAndPassword  } from "firebase/auth";
+import { redirect } from "next/dist/server/api-utils";
+import { useRouter } from "next/navigation";
+
+
 const page = () => {
   const auth = getAuth();
+ const router = useRouter();
   const [nameInput, setNameInput] = useState("");
   const [emailInput, setEmailInput] = useState("");
   const [passwordInput, setPasswordInput] = useState("");
@@ -14,44 +19,39 @@ const page = () => {
   const handleSubmit = (e) => {
     e.preventDefault();
    
-    createUserWithEmailAndPassword(auth, emailInput, passwordInput)
+    signInWithEmailAndPassword(auth, emailInput, passwordInput)
       .then((userCredential) => {
         // Signed up
-       console.log("Accout Create Successfully")
+       console.log("Login Successfully")
+      router.push("/");
       })
       .catch((error) => {
         const errorCode = error.code;
         const errorMessage = error.message;
         console.log( errorCode, errorMessage)
       });
+
   };
 
   return (
     <div>
       <Topbar />
       <Navbar />
-      <div className="container flex justify-between py-10 ">
-        <div className="w-1/2">
+      <div className="container flex justify-between py-10 px-5 gap-5 items-center">
+        <div className="hidden md:block w-1/2">
           <img src="SideImage.png" alt="SideImage" />
         </div>
 
-        <div className="w-1/3">
-          <h2 className="text-3xl font-medium tracking-wider py-2">
-            Create an account
+        <div className="md:w-1/2 ">
+          <h2 className="text-3xl font-bold tracking-wide py-2">
+            Login To Your account
           </h2>
-          <p className="">Enter Details Below</p>
           <form
             onSubmit={handleSubmit}
             action=""
             className="flex flex-col py-5 gap-5"
           >
-            <input
-              type="text"
-              className="border-b-3 border-gray-400 p-2"
-              placeholder="Name"
-              onChange={(e) => setNameInput(e.target.value)}
-              required
-            />
+         
             <input
               type="email"
               className="border-b-3 border-gray-400 p-2"
@@ -66,7 +66,7 @@ const page = () => {
               onChange={(e) => setPasswordInput(e.target.value)}
               required
             />
-            <button className="mt-5 bg-pri text-white rounded-sm w-full py-5">
+            <button className="mt-5 bg-pri text-white rounded-sm w-full py-3">
               Create Account
             </button>
             <button
@@ -74,13 +74,13 @@ const page = () => {
               className="border-2 flex items-center justify-center text-lg gap-5 border-black/20 text-black rounded-sm w-full p-2"
             >
               {" "}
-              <img src="./googleicon.png" alt="googleicon" className="w-10 " />
+              <img src="./googleicon.png" alt="googleicon" className="w-8 " />
               Sign up With Google
             </button>
             <p className="text-gray-500 pb-10 text-center">
-              Already have Account ?{" "}
+             Login Problem ?{" "}
               <a href="" className="underline">
-                Log In
+              Need Help
               </a>
             </p>
           </form>
--- src/app/signup/page.jsx
+++ src/app/signup/page.jsx
@@ -1,13 +1,100 @@
-import Topbar from '@/component/Topbar'
-import React from 'react'
+"use client";
+import Footer from "@/component/Footer";
+import Navbar from "@/component/Navbar";
+import Topbar from "@/component/Topbar";
+import React, { useState } from "react";
+import firebaseConfig from "@/firebase.config";
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import { useRouter } from "next/navigation";
+import Link from "next/link";
 
-const signup = () => {
+
+const page = () => {
+  const auth = getAuth();
+  const route = useRouter();
+  const [nameInput, setNameInput] = useState("");
+  const [emailInput, setEmailInput] = useState("");
+  const [passwordInput, setPasswordInput] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    createUserWithEmailAndPassword(auth, emailInput, passwordInput)
+      .then((userCredential) => {
+        // Signed up
+        console.log("Accout Create Successfully");
+        route.push("/signin")
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.log(errorCode, errorMessage);
+      });
+  };
+
   return (
     <div>
-        {/* top bar */}
-        <Topbar/>
-    </div>
-  )
-}
+      <Topbar />
+      <Navbar />
+      <div className="container flex justify-between py-10 px-5 gap-5 items-center">
+        <div className="hidden md:block w-1/2">
+          <img src="SideImage.png" alt="SideImage" />
+        </div>
 
-export default signup
+        <div className="md:w-1/2 ">
+          <h2 className="text-3xl font-medium tracking-wider py-2">
+            Create an account
+          </h2>
+          <p className="">Enter Details Below</p>
+          <form
+            onSubmit={handleSubmit}
+            action=""
+            className="flex flex-col py-5 gap-5"
+          >
+            <input
+              type="text"
+              className="border-b-3 border-gray-400 p-2"
+              placeholder="Name"
+              onChange={(e) => setNameInput(e.target.value)}
+              required
+            />
+            <input
+              type="email"
+              className="border-b-3 border-gray-400 p-2"
+              placeholder="Email@email.com"
+              onChange={(e) => setEmailInput(e.target.value)}
+              required
+            />
+            <input
+              type="password"
+              className="border-b-3 border-gray-400 p-2 "
+              placeholder="Password"
+              onChange={(e) => setPasswordInput(e.target.value)}
+              required
+            />
+            <button className="mt-5 bg-pri text-white rounded-sm w-full py-3">
+              Create Account
+            </button>
+            <button
+              type={"submit"}
+              className="border-2 flex items-center justify-center text-lg gap-5 border-black/20 text-black rounded-sm w-full p-2"
+            >
+              {" "}
+              <img src="./googleicon.png" alt="googleicon" className="w-8 " />
+              Sign up With Google
+            </button>
+            <p className="text-gray-500 pb-10 text-center">
+              Already have Account ?{" "}
+              <Link href="/signin" className="underline">
+                Log In
+              </Link>
+            </p>
+          </form>
+        </div>
+      </div>
+      <Footer />
+    </div>
+  );
+};
+
+export default page;
