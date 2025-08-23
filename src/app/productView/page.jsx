eb037ad2100c2ec31fc4e8312f920b341836ba5f
almamun0101 <almamunkhan0101@gmail.com>
2025-08-23 23:20:35 +0600

    product right side

--- src/app/productView/page.jsx
+++ src/app/productView/page.jsx
@@ -15,7 +15,7 @@ const ProductView = () => {
 
   return (
     <div className="container mx-auto ">
-      <div className="grid grid-cols-1 lg:grid-cols-12 gap-8 p-6 w-full mx-auto my-20">
+      <div className="grid grid-cols-1 lg:grid-cols-12 gap-8 w-full mx-auto my-20">
         {/* Left Side - Thumbnails */}
         <div className="lg:col-span-2 flex lg:flex-col gap-4 justify-center ">
           {thumbnails.map((img, index) => (
@@ -24,8 +24,9 @@ const ProductView = () => {
               src={img}
               alt="thumbnail"
               onClick={() => setMainImg(img)}
-              className={`w-30 h-30 p-5  rounded-xl cursor-pointer border-2 ${mainImg === img ? "border-black" : "border-gray-200"
-                }`}
+              className={`w-30 h-30 p-5  rounded-xl cursor-pointer border-2 ${
+                mainImg === img ? "border-black" : "border-gray-200"
+              }`}
             />
           ))}
         </div>
@@ -35,7 +36,7 @@ const ProductView = () => {
           <img
             src={mainImg}
             alt="main product"
-            className="w-full py-15 h-[75vh] px-20 object-contain rounded-xl shadow-md bg-black/10" 
+            className="w-full py-15 h-[75vh] px-20 object-contain rounded-xl shadow-md bg-black/10"
           />
         </div>
 
@@ -44,23 +45,29 @@ const ProductView = () => {
           {/* Title & Rating */}
           <div>
             <h2 className="text-2xl font-bold text-gray-800">
-              Nike Air Max Pro 2023
+              Havic HV G-92 Gamepad
             </h2>
-            <div className="flex items-center gap-2 mt-2">
+            <div className="flex justify-between items-center gap-1 mt-2">
               {[...Array(5)].map((_, i) => (
                 <FaStar key={i} className="text-yellow-500" />
               ))}
               <span className="text-gray-500 text-sm">(120 Reviews)</span>
+              <p className="text-green-600 font-medium mt-1">✔ In Stock</p>
             </div>
-            <p className="text-green-600 font-medium mt-1">✔ In Stock</p>
           </div>
 
           {/* Price */}
           <p className="text-3xl font-semibold text-black">$199.00</p>
-
+          <p>
+            PlayStation 5 Controller Skin High quality vinyl with air channel
+            adhesive for easy bubble free install & mess free removal Pressure
+            sensitive.
+          
+          </p>
+          <hr />
           {/* Color Options */}
-          <div>
-            <h3 className="font-medium mb-2">Color:</h3>
+          <div className="flex jbetween items-center gap-6"> 
+            <h3 className="font-medium mb-2">Colous:</h3>
             <div className="flex gap-3">
               <span className="w-8 h-8 rounded-full bg-black cursor-pointer border-2 border-gray-300"></span>
               <span className="w-8 h-8 rounded-full bg-red-500 cursor-pointer"></span>
@@ -69,10 +76,10 @@ const ProductView = () => {
           </div>
 
           {/* Size Options */}
-          <div>
+          <div className="flex jbetween items-center gap-6"> 
             <h3 className="font-medium mb-2">Size:</h3>
             <div className="flex gap-3">
-              {['S', 'M', 'L', 'XL'].map((size) => (
+              {["S", "M", "L", "XL"].map((size) => (
                 <button
                   key={size}
                   className="px-4 py-2 border rounded-lg hover:bg-black hover:text-white transition"
